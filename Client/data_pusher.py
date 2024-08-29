a1999a343cde48a0aa2e69dbecc624fc094647ea
Simone Caporale <simone.caporale@cnhind.com>
2024-08-30 01:43:58 +0200

    Added try catch to avoid crashes if server is not responding

--- Client/data_pusher.py
+++ Client/data_pusher.py
@@ -28,9 +28,12 @@ class DataPusher:
 
         self.last_json = json.dumps(self.last_json_data, default=lambda o: o.__dict__, indent=2)
 
-        response = requests.post(self.remote_server_url, data=self.last_json, headers={"Content-Type": "application/json; charset=utf-8"})
-        if response.status_code == 200:
-            timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-            print(f"{timestamp} - Data successfully sent")
-        else:
-            print(f"Error sending data: HTTP ERROR {response.status_code}")
+        try:
+            response = requests.post(self.remote_server_url, data=self.last_json, headers={"Content-Type": "application/json; charset=utf-8"})
+            if response.status_code == 200:
+                timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+                print(f"{timestamp} - Data successfully sent")
+            else:
+                print(f"Error sending data: HTTP ERROR {response.status_code}")
+        except Exception as e:
+            print(f"Error sending data to server: {e}")
